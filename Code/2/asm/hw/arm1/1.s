4a31e890f0392430ff68440f06d11f5bb0c55136
egirlcatnip <egirlcatnip@gmail.com>
2025-04-08 14:58:49 +0200

    Updated

--- Code/2/asm/hw/arm1/1.s
+++
@@ -1,8 +0,0 @@
-.global mul11
-
-mul11:
-    lsl x1, x0, #3      // x * 2^3  =  8x
-    lsl x2, x0, #1      // x * 2^1  =  2x
-    add x0, x0, x1      // x + x*8  =  9x
-    add x0, x0, x2      // 2x + 9x  = 11x
-    ret
--- Code/2/asm/hw/arm1/2.s
+++
@@ -1,8 +0,0 @@
-.global fnzb              // first non-zero binary
-
-fnzb:
-    clz w1, w0            // count leading zeros
-    cmp w1, #32
-    csel w0, wzr, w0, eq
-    sub w0, #31, w1       // index highest bit
-    ret
